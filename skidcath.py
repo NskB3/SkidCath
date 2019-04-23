69601de706218db5404b04f872764a21f8c7f83a
NskB3 <40596952+NskB3@users.noreply.github.com>
2019-04-23 20:29:10 +0200

    Add files via upload

---
+++ skidcath.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python2
+#encoding=utf8
+######################
+#    RUN AS ROOT.    #
+######################
+__author__ = "Dump"
+
+import socket
+import sys
+import time
+
+try:
+	import colors
+except:
+	print("[-] Colors module not found, ask %s for it!" % __author__)
+	quit()
+def platform_check():
+	platform = sys.platform
+	if "x" not in platform:
+		print(colors.red + "This is only for Linux/Unix systems, sorry!")
+		quit()
+	else:
+		print(colors.green + "[+] Platform check done!")
+def server():
+	logfile = "/etc/SkidCath_LOG.log"
+	s = socket.socket()
+	print(colors.yellow)
+	print("Attempting to bind socket...")
+	time.sleep(0.5)
+	try:
+		s.bind(("", 22))
+	except Exception as e:
+		print(colors.red + "[-] Failed to bind socket, due to:" + str(e) +colors.end)
+		quit()
+	print(colors.green + "[+] Socket binded successfully!")
+	print(colors.yellow + "Attempting to set up listener...")
+	time.sleep(0.5)
+	try:
+		s.listen(10)
+	except Exception as e:
+		print(colors.red + "[-] Failed to set up listener, due to:" + str(e) + colors.end)
+                quit()
+	print(colors.green + "[+] Listener setup done!")
+	print(colors.yellow + "Server runnning, waiting for connections..." + colors.end)
+	while 1:
+		try:
+			conn, addr = s.accept()
+			if addr[0] != "127.0.0.1":
+				print(colors.green + "[LOGGED] {}".format(addr[0]))
+				open(logfile, "a").write(str(addr).split(",")[0].replace('(', '').replace(')', '').replace("'", "") + "\n")
+				reply = "Hey skid. You have been caught by SkidCath. You are a disgrace to humanity, bye."
+				conn.send(reply)
+				conn.close()
+		except: pass
+if __name__ == '__main__':
+	platform_check()
+	server()
